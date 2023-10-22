47ec284a35931d57da9ae5617267b722fac86bac
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-10-22 22:40:18 +0800

    fix: export

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,7 @@
 export * from "./auth";
 
-import { frontUtils } from "@lceihen/front-utils";
+import { request } from "@lceihen/front-utils";
 
-const { request } = frontUtils;
 import { httpsRequestUrl, authConfig } from "@/config";
 
 export const httpsRequest = (param: any) => {
