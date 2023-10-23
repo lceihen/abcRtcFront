6ed360694300b982f713629a0805f8a1b7ba8c60
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-10-23 09:41:04 +0800

    feat: auth url

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,14 +2,14 @@ import { isProd } from "@/utils";
 
 export const authConfig = isProd
   ? {
-      prodUrl: "https://rtc.abclive.cloud/auth/callback",
-      authUrl: "https://auth.abclive.cloud/",
+      prodUrl: "https://rtc.abclive.cloud",
+      authUrl: "https://auth.abclive.cloud/auth/callback",
       secret: "940ce575-577f-4919-acfe-f7b8d2b2ceb9",
       clientId: "0ad69e55-8dd1-45f3-b72a-06dcec411d31",
     }
   : {
-      prodUrl: "http://localhost:5173/auth/callback",
-      authUrl: "http://localhost:3002",
+      prodUrl: "http://localhost:5173",
+      authUrl: "http://localhost:3002/auth/callback",
       secret: "553d4e01120fe401",
       clientId: "553d4e01120fe401",
     };
