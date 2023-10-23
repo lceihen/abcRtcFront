6f9cbfaee8a55f36a7cad3fcec5bed9aa6c47639
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-10-23 16:48:13 +0800

    test: nginx

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,7 @@ import { isProd } from "@/utils";
 
 export * from "./auth";
 
-export const baseUrl = isProd
-  ? "//rtc-server.abclive.cloud/rtc"
-  : "ws://localhost:3001/rtc";
+export const baseUrl = isProd ? "/rtc" : "ws://localhost:3001/rtc";
 
 export const httpsRequestUrl = isProd ? "" : "http://localhost:3000";
 
